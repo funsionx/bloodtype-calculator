cae69939a6fe34f3bc317a641eb37af51211bcaa
Max <agapovxd@yandex.ru>
2023-05-17 16:53:42 +0300

    some error handling + minor fixes

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,17 @@ use crate::probabilities::probabilities;
 fn main() {
     println!("Welcome! Enter your language (0 is for ru or 1 is for en)");
     let mut lang = String::new();
-    io::stdin().read_line(&mut lang).expect("todo");
+    io::stdin().read_line(&mut lang).unwrap_or_else(|err| {
+        println!("Something bad happened: {err} ");
+        exit(1)
+    });
     let lang = lang.trim().parse().unwrap();
     lang_handler(
         &lang,
         "Enter mother's blood type and rhesus factor (example: 4 +):",
         "Введите группу крови и резус-фактор матери (например: 4 +)",
     );
-    
+
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap_or_else(|err| {
         println!("Something bad happened: {err} ");
@@ -27,9 +30,10 @@ fn main() {
 
     let values = calculate(&input);
 
-    let mothers_bloodtype = values[0]
-        .parse()
-        .expect("You should print a number according to your blood type");
+    let mothers_bloodtype = values[0].parse().unwrap_or_else(|err| {
+        println!("Error: {err}\nYou should print a number according to your blood type");
+        exit(1)
+    });
     let mothers_rh = values[1];
 
     lang_handler(
@@ -40,18 +44,24 @@ fn main() {
 
     let mut input2 = String::new();
     io::stdin().read_line(&mut input2).unwrap_or_else(|err| {
-      println!("Something bad happened: {err} ");
-      exit(1)
-  });
+        println!("Something bad happened: {err} ");
+        exit(1)
+    });
 
     let values = calculate(&input2);
 
-    let fathers_bloodtype = values[0]
-        .parse()
-        .expect("You should print a number according to your blood type");
+    let fathers_bloodtype = values[0].parse().unwrap_or_else(|err| {
+        println!("Error: {err}\nYou should print a number according to your blood type");
+        exit(1)
+    });
     let fathers_rh = values[1];
 
-    println!("<------------------------>");
+    if fathers_rh.parse::<i32>().is_ok() | mothers_rh.parse::<i32>().is_ok() {
+        println!("Please write + or - for rhesus factor");
+        exit(1)
+    }
+
+    println!("<--------------------------------------->");
 
     probabilities(
         &lang,
